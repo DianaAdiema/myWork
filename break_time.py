2320e70457e94e23e9a929bcbc4b59ad4e832469
Diana Adiema <adiemadiana2@gmail.com>
2017-12-20 14:16:26 +0300

    On branch master  Initial commit
     Changes to be committed:
    	new file:   break_time.py

---
+++ break_time.py
@@ -0,0 +1,10 @@
+import webbrowser
+import time
+
+play_times=3
+count=0
+
+while (count<play_times):
+ time.sleep(2)   
+ webbrowser.open("https://www.youtube.com/watch?v=Ahi3touX0Ys")
+ count=count+1
