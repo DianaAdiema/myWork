b745f67f8bf36d310467a1156df609a5c32d840d
Diana Adiema <adiemadiana2@gmail.com>
2017-12-27 15:40:24 +0300

    On branch master  Your branch is up to date with 'origin/master'.  Changes to be committed: 	new file:   guess_number.py

---
+++ guess_number.py
@@ -0,0 +1,15 @@
+def Guessing():
+	print "Guess number between 1 and 100"
+	random_number=40
+	found=False
+
+	while not found:
+		number_guess=input("Enter your guess: ")
+		if number_guess==random_number:
+			print "Good!That's it"
+			found= True
+		else:
+			print "That's not it"
+
+Guessing()
+
